2f5d2bb3ee06c07acab940736c583039bae457ca
Caio César Silva <caiocesarsilva08@gmail.com>
2020-05-14 10:11:32 -0300

    Counter Store Actions
    Added and tested 2 new counter Store actions: add_counter and remove_counter.

--- src/redux-flow/reducers/counters/counters.test.js
+++ src/redux-flow/reducers/counters/counters.test.js
@@ -1,16 +1,38 @@
 import counters from "./index";
 import { expect } from "chai";
+import deepFreeze from "deep-freeze";
 
 it("counters should be a function", () => {
   expect(counters).to.be.a("function");
 });
 
-it("should add a counter", () => {
+it("should add a new counter", () => {
   /*If the initial state is an empty array, then when i execute an action ,add_counter in this case,
     we will have an array with an 0 in it, because that's our default state.*/
-
-  const before = [];
-  const action = { type: "ADD_COUNTER" };
+  const before = deepFreeze([]);
+  const action = deepFreeze({ type: "ADD_COUNTER" });
   const after = [0];
+  expect(counters(before, action)).to.be.deep.equal(after);
+});
+
+it("should add a new counter again", () => {
+  const before = deepFreeze([0, 1]);
+  const action = deepFreeze({ type: "ADD_COUNTER" });
+  const after = [0, 1, 0];
+  expect(counters(before, action)).to.be.deep.equal(after);
+});
+
+it("should remove a counter", () => {
+  const before = deepFreeze([0, 1, 2]);
+  const action = deepFreeze({ type: "REMOVE_COUNTER", index: 1 });
+  const after = [0, 2];
+
+  expect(counters(before, action)).to.be.deep.equal(after);
+});
+
+it("should remove a counter again", () => {
+  const before = deepFreeze([3, 1]);
+  const action = deepFreeze({ type: "REMOVE_COUNTER", index: 0 });
+  const after = [1];
   expect(counters(before, action)).to.be.deep.equal(after);
 });
--- src/redux-flow/reducers/counters/index.js
+++ src/redux-flow/reducers/counters/index.js
@@ -1,3 +1,11 @@
 export default (state, action) => {
-  return [0];
+  switch (action.type) {
+    case "ADD_COUNTER":
+      //concat method returns a new array.
+      return state.concat(0);
+    case "REMOVE_COUNTER":
+      /**we use "_" because we need the first parameter to iterate thru the second (index)
+         but we don't use the first one in any moment.*/
+      return state.filter((_, index) => index !== action.index);
+  }
 };
